f64411961e089a791236b08701e0e84c9c2f8346
Joewee Loke <105894547+PuffiezZ@users.noreply.github.com>
2026-01-04 10:17:03 +0700

    Import Low poly
    -Be dummy player character

---
+++ Game Production - Milfairy/Assets/Game Script/Multiplayer Script/Manager Code/RoomManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Conecttting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("testroom",null,null);
+        Debug.Log("Now players joined to lobby");
+    }
+}
