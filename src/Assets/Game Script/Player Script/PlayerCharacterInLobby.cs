85f30c7b9a3cb8c9ae2e70b63c27f0eda03c2151
Joewee Loke <105894547+PuffiezZ@users.noreply.github.com>
2026-03-02 19:07:20 +0700

    Up date Lobby

---
+++ src/Assets/Game Script/Player Script/PlayerCharacterInLobby.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class PlayerCharacterInLobby : MonoBehaviour
+{
+    public SelectCharacter selectCharacter;
+    [SerializeField] private List<ModifyCharacterMesh> modifyCharacterMeshes = new List<ModifyCharacterMesh>();
+    private Dictionary<SelectCharacter,List<SkinnedMeshRenderer>> DictskinnedMeshRenderers = new Dictionary<SelectCharacter,List<SkinnedMeshRenderer>>();
+    
+    public enum SelectCharacter
+    {
+        Character1,
+        Character2
+    }
+    private void OnValidate()
+    {
+        foreach (var each in modifyCharacterMeshes)
+        {
+            DictskinnedMeshRenderers.Add(each.SetSelectCharacter, each.skinClothes);
+        }
+    }
+    [Button("Reset Skin")]
+    public void ResetSkin()
+    {
+        SetPlayerSkin(selectCharacter);
+    }
+    private void SetPlayerSkin(SelectCharacter getSelectCharacter)
+    {
+        foreach(var each in DictskinnedMeshRenderers)
+        {
+            List<SkinnedMeshRenderer> getSkin = each.Value;
+            
+            foreach(var skin in getSkin)
+            {
+                skin.gameObject.SetActive(getSelectCharacter == each.Key);
+            }
+        }
+    }
+}
+[System.Serializable]
+public class ModifyCharacterMesh
+{
+    public PlayerCharacterInLobby.SelectCharacter SetSelectCharacter;
+    public List<SkinnedMeshRenderer> skinClothes;
+}
+
